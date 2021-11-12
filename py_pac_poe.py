6c79b4b0d20b502c4a9a0f2c3c47b8d96701b4b9
Diana Barney <diana.aiken@gmail.com>
2021-11-12 15:17:10 -0500

    initial commit - file contains base user stories

---
+++ py_pac_poe.py
@@ -0,0 +1,41 @@
+'''
+    1. AAP, I want to see a welcome message at the start:
+            ----------------------
+            Let's play Py-Pac-Poe!
+            ----------------------
+    
+    2. AAP, before being prompted for a move, I want to see the board printed out in the console, so that I know what moves have been made:
+                A   B   C
+
+            1)  X |   | O
+            -----------
+            2)    | X |
+            -----------
+            3)  X | O | O
+    
+    3. AAP, I want to be prompted with which player's move it is.
+    
+    4. AAP, I want to be prompted on how to enter a valid move so that I don't make mistakes:
+                A   B   C
+
+            1)  X |   | O
+            -----------
+            2)    | X |
+            -----------
+            3)  X | O | O
+
+        Player X's Move (example B2):
+    
+    5. AAP, I want to be able to enter my move's column letter in upper or lower case (a/A, b/B, c/C) to make it easier to enter my move.
+    
+    6. AAP, if I enter a move in an invalid format, or if I try to occupy a cell already taken, I want to see a message chastising me and be re-prompted:
+            Player X's Move (example B2): Z9
+            Bogus move! Try again...
+
+            Player X's Move (example B2):
+    
+    7. AAP, at the end of a game I want to see who won the game:
+            Player X wins the game!
+        or if it was a tie
+            Another tie!
+'''
